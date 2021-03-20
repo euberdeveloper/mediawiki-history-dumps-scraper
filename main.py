b202a536b3d453afc1fe788078fae4331f70a3a5
euberdeveloper <euberdeveloper@gmail.com>
2021-03-20 20:00:15 +0100

    feat: add whdtscraper lib
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 from datetime import date
 
-from utils import scraper
+import whdtscraper
 
 
 app = FastAPI()
@@ -20,24 +20,24 @@ app.add_middleware(
 
 @app.get('/origin')
 def get_origin():
-    return scraper.WIKI_URL
+    return whdtscraper.WIKI_URL
 
 
 @app.get('/versions/latest')
 def get_latest_version(wikies: bool = False, lang: str = None, wikitype: str = None, dumps: bool = False, start: Optional[date] = None, end: Optional[date] = None):
-    return scraper.fetch_latest_version(wikies=wikies, lang=lang, wikitype=wikitype, dumps=dumps, start=start, end=end)
+    return whdtscraper.fetch_latest_version(wikies=wikies, lang=lang, wikitype=wikitype, dumps=dumps, start=start, end=end)
 
 
 @app.get('/versions')
 def get_versions(wikies: bool = False, lang: str = None, wikitype: str = None, dumps: bool = False, start: Optional[date] = None, end: Optional[date] = None):
-    return scraper.fetch_versions(wikies=wikies, lang=lang, wikitype=wikitype, dumps=dumps, start=start, end=end)
+    return whdtscraper.fetch_versions(wikies=wikies, lang=lang, wikitype=wikitype, dumps=dumps, start=start, end=end)
 
 
 @app.get('/versions/{version}/wikies')
 def get_wikies(version: str, lang: str = None, wikitype: str = None, dumps: bool = False, start: Optional[date] = None, end: Optional[date] = None):
-    return scraper.fetch_wikies(version, lang=lang, wikitype=wikitype, dumps=dumps, start=start, end=end)
+    return whdtscraper.fetch_wikies(version, lang=lang, wikitype=wikitype, dumps=dumps, start=start, end=end)
 
 
 @app.get('/versions/{version}/wikies/{wiki}/dumps')
 def get_dumps(version: str, wiki: str, start: Optional[date] = None, end: Optional[date] = None):
-    return scraper.fetch_dumps(version, wiki, start=start, end=end)
+    return whdtscraper.fetch_dumps(version, wiki, start=start, end=end)
