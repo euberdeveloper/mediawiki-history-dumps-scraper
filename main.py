9e1c8742526930d881630717b69c7cfb5673a8e8
euberdeveloper <euberdeveloper@gmail.com>
2021-07-28 19:15:26 +0200

    style: set pylint
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 from datetime import date
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import whdtscraper
 
