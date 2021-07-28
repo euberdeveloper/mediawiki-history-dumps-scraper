fa76c54aadc02e33397a4bbb32dc8a050b48a70f
euberdeveloper <euberdeveloper@gmail.com>
2021-07-28 19:17:50 +0200

    refactor: use mhdscraper
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import date
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-import whdtscraper
+import mhdscraper
 
 
 app = FastAPI()
@@ -20,24 +20,24 @@ app.add_middleware(
 
 @app.get('/origin')
 def get_origin():
-    return whdtscraper.WIKI_URL
+    return mhdscraper.WIKI_URL
 
 
 @app.get('/versions/latest')
 def get_latest_version(wikies: bool = False, lang: str = None, wikitype: str = None, dumps: bool = False, start: Optional[date] = None, end: Optional[date] = None):
-    return whdtscraper.fetch_latest_version(wikies=wikies, lang=lang, wikitype=wikitype, dumps=dumps, start=start, end=end)
+    return mhdscraper.fetch_latest_version(wikies=wikies, lang=lang, wikitype=wikitype, dumps=dumps, start=start, end=end)
 
 
 @app.get('/versions')
 def get_versions(wikies: bool = False, lang: str = None, wikitype: str = None, dumps: bool = False, start: Optional[date] = None, end: Optional[date] = None):
-    return whdtscraper.fetch_versions(wikies=wikies, lang=lang, wikitype=wikitype, dumps=dumps, start=start, end=end)
+    return mhdscraper.fetch_versions(wikies=wikies, lang=lang, wikitype=wikitype, dumps=dumps, start=start, end=end)
 
 
 @app.get('/versions/{version}/wikies')
 def get_wikies(version: str, lang: str = None, wikitype: str = None, dumps: bool = False, start: Optional[date] = None, end: Optional[date] = None):
-    return whdtscraper.fetch_wikies(version, lang=lang, wikitype=wikitype, dumps=dumps, start=start, end=end)
+    return mhdscraper.fetch_wikies(version, lang=lang, wikitype=wikitype, dumps=dumps, start=start, end=end)
 
 
 @app.get('/versions/{version}/wikies/{wiki}/dumps')
 def get_dumps(version: str, wiki: str, start: Optional[date] = None, end: Optional[date] = None):
-    return whdtscraper.fetch_dumps(version, wiki, start=start, end=end)
+    return mhdscraper.fetch_dumps(version, wiki, start=start, end=end)
